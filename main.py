bd6c13f42d67a110013e46553e9fdf942ecac064
LudwigStumpp <ludwig.stumpp@web.de>
2020-04-16 19:28:44 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import TicTacToe
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TicTacToe',
+    version='0.0',
+    description='Tic Tac Toe game with simple and more advanced agents using MinMax and Reinforcement Learning',
+    url='https://github.com/LudwigStumpp/tic-tac-toe',
+    author='Ludwig Stmupp',
+    author_email='lus7039@thi.de',
+    license='MIT',
+    packages=['TicTacToe'],
+    zip_safe=False)
